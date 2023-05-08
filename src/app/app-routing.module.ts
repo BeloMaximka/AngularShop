f615f8d396ae5f48ae533ca4a7ddef2281ca9c29
BeloMaximka <belomaxim@gmail.com>
2023-05-08 11:03:44 +0300

    add object-fit-contain class, basic admin page layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainPageComponent } from './pages/main-page/main-page.component';
 import { ShoppingCartPageComponent } from './pages/shopping-cart-page/shopping-cart-page.component';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 const routes: Routes = [
   { path: 'shopping-cart', component: ShoppingCartPageComponent },
   { path: 'products', component: MainPageComponent },
+  { path: 'admin', component: AdminPageComponent},
   { path: 'product/:id', component: ProductPageComponent },
   { path: '**', redirectTo: 'products', pathMatch:'full' },
   
--- src/app/pages/admin-page/admin-page.component.ts
+++ src/app/pages/admin-page/admin-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DataAccessService } from 'src/app/database/data-access.service';
+import { Product } from 'src/app/database/models/product';
 
 @Component({
   selector: 'app-admin-page',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-page.component.css']
 })
 export class AdminPageComponent {
+  public product: Product = new Product('', '', '', 0, '');
+  public isImageLoaded = false;
+  public products: Product[] = [];
 
+  constructor(private database: DataAccessService) {
+    this.getProducts()
+  }
+  
+  onImgError() {
+    this.isImageLoaded = false;
+  }
+
+  onImgLoad() {
+    this.isImageLoaded=true;
+  }
+
+  
+  
+  private async getProducts() {
+    this.products = await this.database.getProducts();
+  }
 }
