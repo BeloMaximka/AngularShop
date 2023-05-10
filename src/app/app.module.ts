1fe88519abba8a6a3c765b2db36941a15eadf14b
BeloMaximka <belomaxim@gmail.com>
2023-05-10 20:22:10 +0300

    proper image validation, remove unnecessary module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductCardsModule } from './product-cards/product-cards.module';
 import { PagesModule } from './pages/pages.module';
 import { HttpClientModule } from '@angular/common/http';
 
@@ -14,7 +13,6 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ProductCardsModule,
     PagesModule,
     HttpClientModule
   ],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
-import { CommonModule, FormStyle } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
-import { ProductCardsModule } from '../product-cards/product-cards.module';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
 import { ShoppingCartPageComponent } from './shopping-cart-page/shopping-cart-page.component';
@@ -17,7 +16,6 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    ProductCardsModule,
     FormsModule,
     RouterModule
   ]
